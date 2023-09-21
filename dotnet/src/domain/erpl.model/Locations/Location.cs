0196ae9cc341556d5f53be80e60ad04f5a0cb304
Nikolaos Maliganis <>
2023-09-21 15:06:57 +0200

    WIP : For Update and Deletion

---
+++ dotnet/src/domain/erpl.model/Locations/Location.cs
@@ -0,0 +1,9 @@
+namespace erpl.model.Locations;
+
+public class Location
+{
+    public string Street { get; set; }
+    public string City { get; set; }
+    public string Country { get; set; }
+    public string Postcode { get; set; }
+}
